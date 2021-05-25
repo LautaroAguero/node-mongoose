92a1f6e1144d8f38e36b5c4a712abed1a3659df0
lauta96aguero <lauta96aguero@gmail.com>
2021-05-25 20:56:19 -0300

    Mongoose ODM Part2

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log("Connected correctly to server");
 
-  var newDish = Dishes({
+  Dishes.create({
     name: "Uthapizza",
     description: "test",
-  });
-
-  newDish
-    .save()
+  })
     .then((dish) => {
       console.log(dish);
 
-      Dishes.find({}).exec();
+      return Dishes.find({}).exec();
     })
     .then((dishes) => {
       console.log(dishes);
